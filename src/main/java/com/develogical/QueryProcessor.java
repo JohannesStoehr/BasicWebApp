54affac483291ba831d1f6e9efdc3921b4d1bdca
JoejoeSt <johannes.stoehr@tum.de>
2022-07-05 10:48:57 +0200

    Return query

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -12,6 +12,6 @@ public class QueryProcessor {
         } else if(query.toLowerCase().contains("what is your name")) {
             return "Johannes";
         }
-        return "";
+        return query;
     }
 }
